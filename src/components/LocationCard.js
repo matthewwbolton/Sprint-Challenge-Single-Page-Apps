12ca64108e2fa7c3f9755bbc19dd29089cc3ae41
Matthew Bolton <matthewwbolton@gmail.com>
2020-02-28 12:13:40 -0500

    added new route to locations pages and added new API call to LocationsList component

--- src/components/LocationCard.js
+++ src/components/LocationCard.js
@@ -1,5 +1,76 @@
-import React from "react";
 
-export default function LocationCard({ name, type, dimension, residents }) {
-  return <span>todo: location</span>;
-}
+import React from 'react';
+import {
+  Card, CardImg, CardText, CardBody,
+  CardTitle, CardSubtitle, Button
+} from 'reactstrap';
+
+import styled from 'styled-components';
+
+const NewCard = styled(Card)`
+  width: 30%;
+  margin: 1%;
+`;
+
+const NewDiv = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+`;
+
+const NewCardBody = styled(CardBody)`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const LocationCard = (props) => {
+
+    return (
+      <NewDiv className='character-container'>
+        {props.location.map((elem, i) => (
+        <NewCard key={i}>
+          <NewCardBody>
+            <CardTitle>Location: {elem.name}</CardTitle>
+            <CardSubtitle>Type: {elem.type}</CardSubtitle>
+            <CardText>Dimension: {elem.dimension}</CardText>
+          </NewCardBody>
+        </NewCard>
+        ))}
+      </NewDiv>
+    );
+  };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  //   return (
+//     <div>
+//       {props.location.map((elem, i) => {
+//       <Card key={i}>
+//         <CardBody>
+//           <CardTitle>Location Name: {elem.name}</CardTitle>
+//           <CardSubtitle> Location Type: {elem.type}</CardSubtitle>
+//           <CardText>Location Dimension: {elem.dimension}</CardText>
+//         </CardBody>
+//       </Card>
+//       })}
+//     </div>
+//   );
+// };
+
+export default LocationCard;
--- src/components/LocationsList.js
+++ src/components/LocationsList.js
@@ -1,9 +1,31 @@
 import React, { useEffect, useState } from "react";
+import LocationCard from './LocationCard';
+import axios from 'axios';
 
 const LocationsList = () => {
+
+    const [location, setLocation] = useState([]);
+
+  useEffect(() => {
+    axios.get('https://cors-anywhere.herokuapp.com/https://rickandmortyapi.com/api/location/')
+         .then(res => {
+           console.log(res.data.results);
+           setLocation(res.data.results)
+         })
+         .catch(err => {
+           console.log(err)
+         })
+  }, [])
+
+
+  return (
+    <div>
+      <LocationCard location={location}/>
+    </div>
+
+  );
+}
     
-    
-    return;
-};
+
 
 export default LocationsList;
