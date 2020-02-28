120b5a4f4f6cae2c86a3ca00fa2f71aa3639e9e5
Matthew Bolton <matthewwbolton@gmail.com>
2020-02-28 11:52:46 -0500

    finished styling homepage and header components

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,29 @@
 import React from "react";
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+
+const NewDiv = styled.div`
+  display: flex;
+  justify-content: center;
+`;
+
+const NewLink = styled(Link)`
+  margin: 2%
+`;
+
+
+
+
 
 export default function Header() {
   return (
     <header className="ui centered">
       <h1 className="ui center">Rick &amp; Morty Fan Page</h1>
+      <NewDiv>
+      <NewLink to='/'>Home</NewLink> 
+      <NewLink to='/characters'>Characters</NewLink>
+      <NewLink to='/locations'>Locations</NewLink>
+      </NewDiv>
     </header>
   );
 }
--- src/components/WelcomePage.js
+++ src/components/WelcomePage.js
@@ -2,21 +2,31 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 
+const NewHeader = styled.header`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const NewImg = styled.img`
+  width: 50%;
+  border-radius: 50%;
+  
+`;
+
 export default function WelcomePage() {
   return (
-    <section className="welcome-page">
-      <Link to='/'>Home</Link> 
-      <Link to='/characters'>Characters</Link>
-      <Link to='/locations'>Locations</Link>
-      <header>
+    <section >
+   
+      <NewHeader>
         <h1>Welcome to the ultimate fan site!</h1>
-        <img
+        <NewImg
           className="main-img"
           src="https://rickandmortyapi.com/api/character/avatar/1.jpeg"
           alt="rick"
         />
         
-      </header>
+      </NewHeader>
     </section>
   );
 }
