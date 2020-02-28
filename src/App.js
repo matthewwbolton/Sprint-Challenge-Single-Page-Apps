d3474392d862bf8d60d05b988dd9bc0f56980573
Matthew Bolton <matthewwbolton@gmail.com>
2020-02-28 11:10:10 -0500

    setup routes and links with React Router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
 import React from "react";
 import Header from "./components/Header.js";
+import { Route } from 'react-router-dom';
+import WelcomePage from './components/WelcomePage';
+import CharacterList from './components/CharacterList';
+import LocationsList from './components/LocationsList';
+
+
 
 
 export default function App() {
+
+  
+
+  
+
   return (
     <main>
       <Header />
+      <Route exact path='/'>
+        <WelcomePage />
+      </Route>
+      <Route path='/characters'>
+        <CharacterList />
+      </Route>
+      <Route path='/locations'>
+        <LocationsList />
+      </Route>
     </main>
   );
-}
+};
--- src/components/WelcomePage.js
+++ src/components/WelcomePage.js
@@ -1,8 +1,13 @@
-import React from "react";
+import React from 'react';
+import { Link } from 'react-router-dom';
+import stlyled from 'styled-components';
 
 export default function WelcomePage() {
   return (
     <section className="welcome-page">
+      <Link to='/'>Home</Link> 
+      <Link to='/characters'>Characters</Link>
+      <Link to='/locations'>Locations</Link>
       <header>
         <h1>Welcome to the ultimate fan site!</h1>
         <img
@@ -10,6 +15,7 @@ export default function WelcomePage() {
           src="https://rickandmortyapi.com/api/character/avatar/1.jpeg"
           alt="rick"
         />
+        
       </header>
     </section>
   );
